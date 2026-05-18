0c56cd529a9d6e48161448181c0f1ec65bd4c40d
Infinite1st <max24107@gmail.com>
2026-05-18 09:41:35 +0300

    Update PCH.h

--- src/PCH.h
+++ src/PCH.h
@@ -1,6 +1,7 @@
 #pragma once
-
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
+#include <spdlog/spdlog.h>
 #include "log.h"
 using namespace std::literals;
+#define logger spdlog::default_logger_raw()
