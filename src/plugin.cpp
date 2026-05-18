bfcdf86290daca5d4d818c1aa39c51a7bfdca239
Infinite1st <max24107@gmail.com>
2026-05-18 09:49:20 +0300

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -29,7 +29,6 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse)
     SKSE::Init(skse);
     SetupLog();
 
-    logger::info("PlayerCount loading...");
     Config::Get().Load();
 
     auto* messaging = SKSE::GetMessagingInterface();
@@ -37,6 +36,5 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse)
         return false;
     }
 
-    logger::info("PlayerCount loaded.");
     return true;
 }
